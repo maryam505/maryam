855a8a80483a1c37a75e8efedbe8dc2a7af9bf41
maryam505 <63542@students.riphah.edu.pk>
2024-11-01 13:21:19 +0500

    Add files via upload

---
+++ pf lab task 2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main ()
+{
+string name,course;
+int semester,sapid;
+cout<<"name";
+cin>>name;
+cout<<"sapid";
+cin>>sapid;
+cout<<"course";
+cin>>course;
+cout<<"semester";
+cin>>semester;
+return 0;
+}
